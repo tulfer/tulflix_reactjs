4e54b7ec00b1fc050634a9f6d853d58661a3a751
Tulfer <tulfer01@gmail.com>
2018-11-26 14:34:06 -0500

    Hola Mundo
    Empezando con React Js

---
+++ src/Saludo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class Saludo extends Component {
+  render() {
+    return (
+      <h1>
+        Hola Mundo!
+      </h1>
+    );
+  }
+}
+ 
+export default Saludo;
